4c5fa4ae70c08edc50ab0f1bb85114078ddfc1b6
Mahmoud-Br <87929176+Mahmoud-Br@users.noreply.github.com>
2022-07-18 23:31:39 +0200

    Add files via upload

--- 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challenges/Strings-challenge.py
+++ 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challenges/Strings-challenge.py
@@ -1,1 +1,5 @@
-
+miles = input('Enter a distance in miles: ')
+# kilometers_value = miles_value * 1.609344
+miles_num = float(miles)
+kilometers = miles_num * 1.609344
+print("The distance in kilometers is : ", kilometers)
