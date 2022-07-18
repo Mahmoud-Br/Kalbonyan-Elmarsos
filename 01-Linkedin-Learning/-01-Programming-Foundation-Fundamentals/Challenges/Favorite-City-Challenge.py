ab5e6298902d6944d5a0489a7709a0c6607494f1
Mahmoud-Br <87929176+Mahmoud-Br@users.noreply.github.com>
2022-07-18 23:22:36 +0200

    Delete Favorite-City-Challenge.py

--- 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Favorite-City-Challenge.py
+++
@@ -1,9 +0,0 @@
-#print("What is my favorite food: ")
-myFavoritefood = "pizza"
-answer = input("What is my favorite food: ")
-if answer == myFavoritefood:
-    print("Yep! So amazing!")
-else:
-    print("Yuck! That's not it!")
-
-print("Thanks for playing!")
